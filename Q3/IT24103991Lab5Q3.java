2713910f17e50c5371dabf0bc8061ba770cecc82
IT24103991 <it24103991@MY.SLIIT.LK>
2024-11-28 15:12:09 +0530

    Update IT24103991Lab5Q3.java

--- Q3/IT24103991Lab5Q3.java
+++ Q3/IT24103991Lab5Q3.java
@@ -31,18 +31,17 @@ public class IT24103991lab5Q3 {
         double totalCharge = daysReserved * ROOM_CHARGE_PER_DAY;
         double discountRate = 0;
 
-        // Determine discount rate based on number of days
+        
         if (daysReserved >= 3 && daysReserved <= 4) {
             discountRate = DISCOUNT_3_TO_4_DAYS;
         } else if (daysReserved >= 5) {
             discountRate = DISCOUNT_5_OR_MORE_DAYS;
         }
 
-        // Calculate total amount after discount
+        
         double discountAmount = (totalCharge * discountRate) / 100;
         double finalAmount = totalCharge - discountAmount;
 
-        // Display the results
         System.out.println("Number of days reserved: " + daysReserved);
         System.out.println("Room charged per day: Rs " + ROOM_CHARGE_PER_DAY);
         System.out.println("Total amount to be paid after discount: Rs " + finalAmount);
